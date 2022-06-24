1660b105159f1826476152fa68026d6b47c4e98a
SOLID-Code-IT <Ant_skvortsov@mail.ru>
2022-06-24 17:02:10 +0500

    git commit Pomidoro

--- src/main/java/SOLID_Code_IT/Pomodoro.java
+++ src/main/java/SOLID_Code_IT/Pomodoro.java
@@ -2,6 +2,6 @@ package SOLID_Code_IT;
 
 public class Pomodoro {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello !!!!");
     }
 }
